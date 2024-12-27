273f586e0ede20de5f041b4143edac7cd5dacd2c
saquibe <saquibe143@gmail.com>
2024-12-27 17:57:03 +0530

    prop drilling

--- src/index.js
+++ src/index.js
@@ -18,26 +18,27 @@ const books = [
 ];
 
 const BookList = () => {
+  const someValue = "hello world";
+  const displayValue = () => {
+    console.log(someValue);
+  };
   return (
     <section className="booklist">
       {books.map((book) => {
         // return <Book book={book} key={book.id} />;
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { title, author, img } = props;
-  const displayTitle = () => {
-    console.log(title);
-  };
+  const { title, author, img, displayValue } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h1>{title}</h1>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={displayValue}>click me</button>
       <h4>{author}</h4>
     </article>
   );
