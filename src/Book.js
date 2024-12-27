074264c978a871074eb4b93735f50c8b1991eb28
saquibe <saquibe143@gmail.com>
2024-12-27 19:39:16 +0530

    add title

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,14 @@
 const Book = (props) => {
   const { title, author, img, number } = props;
   return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <span className="number">{`# ${number + 1}`}</span>
-    </article>
+    <>
+      <article className="book">
+        <img src={img} alt={title} />
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+        <span className="number">{`# ${number + 1}`}</span>
+      </article>
+    </>
   );
 };
 
