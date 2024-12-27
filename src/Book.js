cfb7ad41996dc26654035b5f7346f76be5b50962
saquibe <saquibe143@gmail.com>
2024-12-27 19:31:46 +0530

    numbers challenges

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,11 @@
 const Book = (props) => {
-  const { title, author, img } = props;
+  const { title, author, img, number } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <span className="number">{`# ${number + 1}`}</span>
     </article>
   );
 };
