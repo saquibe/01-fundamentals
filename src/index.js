1710718919c99f7d9e9dd56fc093161d1ee18da5
saquibe <saquibe143@gmail.com>
2024-12-28 20:01:03 +0530

    develop backroads-app

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { books } from "./books";
-import Book from "./Book";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const BookList = () => {
-  return (
-    <>
-      <h1>amazon best sellers</h1>
-      <section className="booklist">
-        {books.map((book, index) => {
-          return <Book {...book} key={book.id} number={index} />;
-        })}
-      </section>
-    </>
-  );
-};
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <div>
-    <BookList />
-  </div>
-);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
